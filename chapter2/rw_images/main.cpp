eec3174693e7c76e3c120d24d404339b44907d78
Sean Wang <xyseanwang@hotmail.com>
2022-07-10 22:38:26 +0800

    feature: use gtk

--- chapter2/rw_images/main.cpp
+++ chapter2/rw_images/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 using namespace std;
 
 // OpenCV includes
@@ -8,10 +8,33 @@ using namespace std;
 #include "opencv2/highgui.hpp"
 using namespace cv;
 
-int main(int argc, const char** argv)
-{
-    // Read images
-    Mat color = imread("../lena.jpg");
-    Mat gray = imread("../lena.jpg, CV_LOAD_IMAGE_GRAYSCALE");
-    return 0;
+int main(int argc, const char** argv) {
+  // Read images
+  Mat color = imread("../lena.jpg");
+  Mat gray = imread("../lena.jpg", IMREAD_GRAYSCALE);
+
+  if (!color.data)  // Check for valid input
+  {
+    cout << "Could not open or find the image" << std::endl;
+    return -1;
+  }
+
+  // Write images
+  imwrite("lenaGray.jpg", gray);
+
+  // Get same pixel with opencv function
+  int myRow = color.cols - 1;
+  int myCol = color.rows - 1;
+
+  Vec3b pixel = color.at<Vec3b>(myRow, myCol);
+  cout << "Pixel value (G,B,R): (" << (int)pixel[0] << (int)pixel[1]
+       << (int)pixel[2] << ")" << std::endl;
+
+  // Show images
+  imshow("Lena BGR", color);
+  imshow("Lena Gray", gray);
+
+  // wait for any key press
+  waitKey(0);
+  return 0;
 }
